d66ad07be821019f49c8d4fd6ccc7ba50cd45e6a
Marino Missiroli <marino.missiroli@desy.de>
2019-10-28 11:46:54 +0100

    updated makeMETPlots.sh

--- makeMETPlots.sh
+++ makeMETPlots.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NoPU=/home/missirol/Desktop/VBF_HToInvisible_M125_14TeV_NoPU_temp.root
-PU140=/home/missirol/Desktop/VBF_HToInvisible_M125_14TeV_PU140_temp.root
-PU200=/home/missirol/Desktop/VBF_HToInvisible_M125_14TeV_PU200_temp.root
+NoPU=/home/missirol/Desktop/jmeTriggerNTuple_VBF_HToInvisible_M125_14TeV_NoPU.root
+PU140=/home/missirol/Desktop/jmeTriggerNTuple_VBF_HToInvisible_M125_14TeV_PU140.root
+PU200=/home/missirol/Desktop/jmeTriggerNTuple_VBF_HToInvisible_M125_14TeV_PU200_temp.root
 
-ODIR=output_191027
+ODIR=output_191028
 
 ### ----------
 
@@ -27,4 +27,4 @@ mkdir -p ${ODIR}
  -o ${ODIR}/plots \
  -e png pdf root
 
-unset -v ODIR
+unset -v NoPU PU140 PU200 ODIR
