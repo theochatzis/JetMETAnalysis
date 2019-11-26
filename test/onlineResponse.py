d0fb6aff68c5b571801110e685a22127b9926965
Marino Missiroli <marino.missiroli@desy.de>
2019-11-26 18:38:12 +0100

    updated test/onlineResponse.py

--- test/onlineResponse.py
+++ test/onlineResponse.py
@@ -5,13 +5,13 @@ import math
 import array
 
 def deltaPhi(phi1, phi2):
-     PHI = abs(phi1-phi2)
-     if (PHI<=3.14159265):
-         return PHI
-     else:        
-         return 2*3.14159265-PHI
+    PHI = abs(phi1-phi2)
+    if (PHI<=math.pi):
+        return PHI
+    else:
+        return 2*math.pi-PHI
 
-def deltaR(eta1, phi1, eta2, phi2) :
+def deltaR(eta1, phi1, eta2, phi2):
     deta = eta1-eta2
     dphi = deltaPhi(phi1,phi2)
     return math.sqrt(deta*deta + dphi*dphi)
@@ -19,7 +19,9 @@ def deltaR(eta1, phi1, eta2, phi2) :
 #xbin = [10,15,18,20,22,25,30,35,40,45,50,60,75,100,160]
 xbin = [5,10,15,17,18,19,20,25,30,35,40,45,50,55,60,80,100,150,200]
 xbins = array.array('d', xbin)
-jetType = "Puppi"#empty or "Corrected" or CHS or CHSCorrected or Puppi for offline
+
+JetCollection = 'slimmedJets'
+jetType = 'CHS' # empty or "Corrected" or CHS or CHSCorrected or Puppi for offline
 
 def histograms(files):
 
@@ -62,22 +64,23 @@ def histograms(files):
 #                         resp[eta_thresholds[0]].Fill(genJet.et(), jetEt / (genJet.et()))
 #               respvseta.Fill(genJet.eta(), jetEt / (genJet.et()) )
 
-          event.getByLabel(("slimmedJetsPuppi"), HLTJetHandle)
+          event.getByLabel(JetCollection, HLTJetHandle)
           l2Jets = HLTJetHandle.product()
-          deltaR_min = 0.4
-          jetEt = -1000
+          deltaR_min = 0.1
+          jetPt = -1000
           for jet in l2Jets:
+              if abs(jet.eta()) > 1.3: continue
               if jet.pt() < 20: continue
               genJet = jet.genJet()
               if not genJet: continue
-              if genJet.pt() < 40: continue
-              if genJet.pt() > 200: continue
+              if genJet.pt() < 20: continue
+#              if genJet.pt() > 200: continue
               deltar = deltaR(genJet.eta(), genJet.phi(), jet.eta(), jet.phi()) 
               if deltar < deltaR_min:
                  deltaR_min = deltar
                  jetPt = jet.pt()
-              deltaRMin.Fill(deltaR_min)
               if jetPt < -20: continue
+              deltaRMin.Fill(deltaR_min)
               if abs(genJet.eta()) > eta_thresholds[0] and abs(genJet.eta()) < eta_thresholds[1]:
 #                 print genJet.pt(), jet.pt(), jet.correctedP4(0).pt(), jet.correctedP4(1).pt(), jet.correctedP4(2).pt() #, jet.correctedP4(3).pt()
                  resp[eta_thresholds[0]].Fill(genJet.pt(), jetPt / (genJet.pt()))
@@ -118,7 +121,9 @@ ROOT.gStyle.SetOptStat(0)
 #ifile = "file:RelValTTbar_14TeV_MINIAODSIM.root"
 
 #ifile = "root://xrootd-cms.infn.it//store/mc/PhaseIITDRSpring19MiniAOD/TT_TuneCP5_14TeV-powheg-pythia8/MINIAODSIM/PU200_106X_upgrade2023_realistic_v3-v1/240000/7A20ACE5-523B-8244-BDC4-0DEC2671DB71.root"
-ifile = "file:TT_14TeV_PU200_MINIAODSIM.root"
+#ifile = "file:TT_14TeV_PU200_MINIAODSIM.root"
+
+ifile = "root://xrootd-cms.infn.it//store/mc/PhaseIITDRSpring19MiniAOD/TT_TuneCP5_14TeV-powheg-pythia8/MINIAODSIM/NoPU_106X_upgrade2023_realistic_v3-v2/40000/37220C36-515A-FA4A-AF94-2349FF426578.root"
 
 myhistos = histograms([ifile])
 newfile = ROOT.TFile("jets.root","RECREATE")
