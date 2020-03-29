994fd13bd496c370f245dccf791a489eed642a56
Marino Missiroli <marino.missiroli@desy.de>
2020-03-29 17:35:30 +0200

    [JMETrigger] updated test/scripts/

---
+++ JMETrigger/test/scripts/plots_run3test_pfBlockAlgoRemovePS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+inpdir=${JMEANA_BASE}/analysis_output_run3_v04/harvesting
+outdir=plots_run3test_pfBlockAlgoRemovePS
+
+samples=(
+ Run3Winter20_QCD_Pt_15to3000_Flat_14TeV
+ Run3Winter20_QCD_Pt_50to80_14TeV
+ Run3Winter20_QCD_Pt_170to300_14TeV
+ Run3Winter20_DYToLL_M50_14TeV
+ Run3Winter20_ZprimeToMuMu_M6000_14TeV
+)
+
+if [ ! -f ${outdir%%/*}.tar.gz ]; then
+
+  for sample in "${samples[@]}"; do
+
+    outd_i=${outdir}/${sample}
+
+    jmePlots_run3.py -o ${outd_i} -l ${sample} -e pdf png root -i \
+     ${inpdir}/HLT/${sample}.root:'Run-2':1 \
+     ${inpdir}/HLT_pfBlockAlgoRemovePS/${sample}.root:'PFBlock w/o PS':2
+
+    unset -v outd_i
+  done
+  unset -v sample
+
+  tar cfz ${outdir%%/*}.tar.gz ${outdir%%/*}
+  rm -rf ${outdir%%/*}
+fi
+
+unset -v inpdir outdir samples
